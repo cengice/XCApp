1dce989ff06599a13cfd9f1eed0966473514a81a
Haris Cengic <Haris.Cengic@yum.com>
2022-12-23 22:07:56 -0500

    Initial Commit

---
+++ my-first1/my_first1App.swift
@@ -0,0 +1,17 @@
+//
+//  my_first1App.swift
+//  my-first1
+//
+//  Created by Addie on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct my_first1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
